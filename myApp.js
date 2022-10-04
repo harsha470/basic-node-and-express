432e54ee6aeb22840992c647228e70051671eb5b
harsha470 <harshamadasi@gmail.com>
2022-10-05 00:45:52 +0530

    used express and static css file

--- myApp.js
+++ myApp.js
@@ -1,13 +1,16 @@
 let express = require('express');
 let app = express();
+var path = require('path') 
 
 // console.log("Hello World") ;        
 // var absPath = __dirname + "/views/index.html" ; 
 
+app.use("/public",express.static(__dirname + "/public")) ; 
 app.get("/", (req,res)=>
 {
     res.sendFile(__dirname + "/views/index.html") ; 
 }); 
+
 
 
 
