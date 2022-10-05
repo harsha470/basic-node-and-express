16bbf48d6ab28eebb01a51db8eeaa21ca41512fa
harsha470 <harshamadasi@gmail.com>
2022-10-05 13:47:07 +0530

    n

--- myApp.js
+++ myApp.js
@@ -1,16 +1,26 @@
 let express = require('express');
 let app = express();
-var path = require('path') 
+var path = require('path') ;
+var dotenv = require('dotenv').config() ; 
 
 // console.log("Hello World") ;        
 // var absPath = __dirname + "/views/index.html" ; 
 
-app.use("/public",express.static(__dirname + "/public")) ; 
+app.use("/public", express.static(__dirname + "/public")) ; 
 app.get("/", (req,res)=>
 {
     res.sendFile(__dirname + "/views/index.html") ; 
 }); 
 
+var obj = {"message": "Hello json"} ; 
+app.get("/json",(req,res)=>
+{
+    if(process.env.MESSAGE_STYLE === "uppercase")
+    {
+        obj["message"] = "HELLO JSON" ; 
+    }
+    res.json(obj) ; 
+})
 
 
 
